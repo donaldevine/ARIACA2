55037fb9c0b6bfc14abbc5a1e256176b97b7e2a4
donaldevine <donaldevine@gmail.com>
2014-03-26 19:41:36 +0000

    Added skeleton calculator implementation

---
+++ js/calculator.js
@@ -0,0 +1,21 @@
+var Calculator = (function() {
+
+    // Public methods
+    return {
+        add: function(numA, numB) {
+            
+        },
+
+        subtract: function(numA, numB) {
+           
+        },
+
+        divide: function(numA, numB) {
+           
+        }
+
+        multiply: function(numA, numB) {
+           
+        }
+    }
+}())
